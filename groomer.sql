68c242649d3998bbe037d428e90656fbd1fb4708
Mingchi Zhang <hlsnw2006@163.com>
2018-05-15 17:00:17 +1000

    Modified database.

--- groomer.sql
+++ groomer.sql
@@ -35,3 +35,18 @@ CREATE TABLE `dinfo` (
   KEY `d_ownerid` (`d_ownerid`),
   CONSTRAINT `dinfo_ibfk_1` FOREIGN KEY (`d_ownerid`) REFERENCES `uinfo` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `appointment`;
+CREATE TABLE `appointment` (
+  `Id` int(10) NOT NULL AUTO_INCREMENT,
+  `a_userid` int(10) NOT NULL DEFAULT '0',
+  `a_variety` varchar(10) NOT NULL DEFAULT '',
+  `a_date` varchar(20) NOT NULL DEFAULT '',
+  `a_time` varchar(50) NOT NULL DEFAULT '',
+  `a_options` varchar(20) NOT NULL DEFAULT '',
+  `a_description` varchar(200) NOT NULL DEFAULT '',
+  PRIMARY KEY (`Id`),
+  KEY `a_userid` (`a_userid`),
+  CONSTRAINT `ainfo_ibfk_1` FOREIGN KEY (`a_userid`) REFERENCES `uinfo` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
